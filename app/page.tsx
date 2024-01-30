d2c5ddb7689361655cf5a35417c3ecaf57814fef
Arti Villa <artivillachandok@gmail.com>
2024-01-29 21:10:36 -0800

    update: our expertise styles in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -13,61 +13,78 @@ export default function Page() {
         companies.
       </p>
       <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-      <h2 className="font-medium text-xl mb-1 tracking-tighter">
-        **Our Expertise:**{" "}
+      <h2 className="font-medium text-2xl mb-8 tracking-tighter">
+        Our Expertise
       </h2>
       <ul>
-        <li>
-          KPI Reports:** Key Performance Indicators (KPIs) are the compass
-          guiding your business toward success. We assist you in defining a
-          singular Key Performance Indicator (KPI) and delivering actionable
-          insights.We assist you in defining a singular Key Performance
-          Indicator (KPI) and delivering actionable insights.
+        <li className="mb-4">
+          <h3 className="inline-block text-lg mb-3">KPI Reports</h3>
+          <p>
+            Key Performance Indicators (KPIs) are the compass guiding your
+            business toward success. We assist you in defining a singular Key
+            Performance Indicator (KPI) and delivering actionable insights.We
+            assist you in defining a singular Key Performance Indicator (KPI)
+            and delivering actionable insights.
+          </p>
         </li>
-        <li>
-          Pattern Recognition:** Uncover hidden patterns and trends within your
-          data with our advanced pattern recognition algorithms. Whether it's
-          identifying customer behavior or optimizing operations, we turn
-          complex datasets into meaningful narratives.
+        <li className="mb-4">
+          <h3 className="inline-block text-lg mb-3">Pattern Recognition</h3>
+          <p>
+            Uncover hidden patterns and trends within your data with our
+            advanced pattern recognition algorithms. Whether it's identifying
+            customer behavior or optimizing operations, we turn complex datasets
+            into meaningful narratives.
+          </p>
         </li>
-        <li>
-          Business Intelligence:** Elevate your decision-making process with our
-          robust business intelligence solutions. We transform raw data into
-          visually engaging reports and dashboards, making it easy for you to
-          grasp critical insights at a glance.
+        <li className="mb-4">
+          <h3 className="inline-block text-lg mb-3">Business Intelligence</h3>
+          <p>
+            Elevate your decision-making process with our robust business
+            intelligence solutions. We transform raw data into visually engaging
+            reports and dashboards, making it easy for you to grasp critical
+            insights at a glance.
+          </p>
         </li>
-        <li>
-          ETL Script Development:** Efficient data processing is the backbone of
-          successful analytics. We develop customized Extract, Transform, Load
-          (ETL) scripts, ensuring seamless data integration, data cleanliness,
-          and completeness.
+        <li className="mb-4">
+          <h3 className="inline-block text-lg mb-3">ETL Script Development</h3>
+          <p>
+            Efficient data processing is the backbone of successful analytics.
+            We develop customized Extract, Transform, Load (ETL) scripts,
+            ensuring seamless data integration, data cleanliness, and
+            completeness.
+          </p>
         </li>
-        <li>
-          Data Pipeline Management:** Streamline your data workflow with our
-          end-to-end data pipeline solutions. From data extraction to storage
-          and analysis, we create efficient and scalable pipelines that adapt to
-          the evolving needs of your business.
+        <li className="mb-4">
+          <h3 className="inline-block text-lg mb-3">
+            Data Pipeline Management
+          </h3>
+          <p>
+            Streamline your data workflow with our end-to-end data pipeline
+            solutions. From data extraction to storage and analysis, we create
+            efficient and scalable pipelines that adapt to the evolving needs of
+            your business.
+          </p>
         </li>
       </ul>
       <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
       <h2 className="font-medium text-xl mb-1 tracking-tighter">
-        Why DataFish Labs?{" "}
+        Why DataFish Labs?
       </h2>
       <ul>
         <li>
-          Tailored Solutions:** We understand that every business is unique. Our
+          Tailored Solutions: We understand that every business is unique. Our
           solutions are customized to align with your specific goals, ensuring
           maximum impact and relevance.
         </li>
         <li>
-          Cutting-Edge Technology:** We stay at the forefront of technological
+          Cutting-Edge Technology: We stay at the forefront of technological
           advancements. Our team leverages the latest tools and methodologies to
           deliver state-of-the-art analytics solutions.
         </li>
         <li>
-          Data Security:** Your data's security is our top priority. We
-          implement robust measures to safeguard your information, providing you
-          with peace of mind.
+          Data Security: Your data's security is our top priority. We implement
+          robust measures to safeguard your information, providing you with
+          peace of mind.
         </li>
       </ul>
     </section>
