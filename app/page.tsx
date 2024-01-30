49780092074edff99de9fef9473edd4620460772
Arti Villa <artivillachandok@gmail.com>
2024-01-29 20:57:08 -0800

    update: prettier formatted

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,74 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">DataFish Labs</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        DataFish Labs
+      </h1>
       <p className="prose prose-neutral dark:prose-invert">
-      At DataFish Labs, we understand that in the dynamic landscape 
-      of today's business world, data is more than just 
-      information – it's a powerful asset waiting to be 
-      harnessed. We are a burgeoning team in the field of 
-      data analytics and insights, founded by seasoned data 
-      engineers and analysts with extensive experience in 
-      leading technology and publishing companies.
+        At DataFish Labs, we understand that in the dynamic landscape of today's
+        business world, data is more than just information – it's a powerful
+        asset waiting to be harnessed. We are a burgeoning team in the field of
+        data analytics and insights, founded by seasoned data engineers and
+        analysts with extensive experience in leading technology and publishing
+        companies.
       </p>
       <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-      <h2 className="font-medium text-xl mb-1 tracking-tighter">**Our Expertise:** </h2>
+      <h2 className="font-medium text-xl mb-1 tracking-tighter">
+        **Our Expertise:**{" "}
+      </h2>
       <ul>
-          <li>
-            KPI Reports:** Key Performance Indicators (KPIs) are the 
-            compass guiding your business toward success. We assist you in 
-            defining a singular Key Performance Indicator (KPI) and delivering 
-            actionable insights.We assist you in defining a singular Key 
-            Performance Indicator (KPI) and delivering actionable insights.
-          </li> 
-          <li>
-            Pattern Recognition:** Uncover hidden patterns and trends 
-            within your data with our advanced pattern recognition algorithms. 
-            Whether it's identifying customer behavior or optimizing operations, 
-            we turn complex datasets into meaningful narratives.
-          </li>
-          <li>
-            Business Intelligence:** Elevate your decision-making process 
-            with our robust business intelligence solutions. We transform raw 
-            data into visually engaging reports and dashboards, making it easy 
-            for you to grasp critical insights at a glance.
-          </li>
-          <li>
-            ETL Script Development:** Efficient data processing is the 
-            backbone of successful analytics. We develop customized Extract, 
-            Transform, Load (ETL) scripts, ensuring seamless data integration,
-            data cleanliness, and completeness.
-          </li>
-          <li>
-            Data Pipeline Management:** Streamline your data workflow with 
-            our end-to-end data pipeline solutions. From data extraction to 
-            storage and analysis, we create efficient and scalable pipelines that 
-            adapt to the evolving needs of your business.
-          </li>
+        <li>
+          KPI Reports:** Key Performance Indicators (KPIs) are the compass
+          guiding your business toward success. We assist you in defining a
+          singular Key Performance Indicator (KPI) and delivering actionable
+          insights.We assist you in defining a singular Key Performance
+          Indicator (KPI) and delivering actionable insights.
+        </li>
+        <li>
+          Pattern Recognition:** Uncover hidden patterns and trends within your
+          data with our advanced pattern recognition algorithms. Whether it's
+          identifying customer behavior or optimizing operations, we turn
+          complex datasets into meaningful narratives.
+        </li>
+        <li>
+          Business Intelligence:** Elevate your decision-making process with our
+          robust business intelligence solutions. We transform raw data into
+          visually engaging reports and dashboards, making it easy for you to
+          grasp critical insights at a glance.
+        </li>
+        <li>
+          ETL Script Development:** Efficient data processing is the backbone of
+          successful analytics. We develop customized Extract, Transform, Load
+          (ETL) scripts, ensuring seamless data integration, data cleanliness,
+          and completeness.
+        </li>
+        <li>
+          Data Pipeline Management:** Streamline your data workflow with our
+          end-to-end data pipeline solutions. From data extraction to storage
+          and analysis, we create efficient and scalable pipelines that adapt to
+          the evolving needs of your business.
+        </li>
       </ul>
       <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-      <h2 className="font-medium text-xl mb-1 tracking-tighter">Why DataFish Labs? </h2>
+      <h2 className="font-medium text-xl mb-1 tracking-tighter">
+        Why DataFish Labs?{" "}
+      </h2>
       <ul>
-          <li>
-            Tailored Solutions:** We understand that every business is 
-            unique. Our solutions are customized to align with your specific 
-            goals, ensuring maximum impact and relevance.
-          </li>
-          <li>
-            Cutting-Edge Technology:** We stay at the forefront of 
-            technological advancements. Our team leverages the latest tools 
-            and methodologies to deliver state-of-the-art analytics solutions.
-          </li>
-          <li>
-            Data Security:** Your data's security is our top priority. 
-            We implement robust measures to safeguard your information, 
-            providing you with peace of mind.
-          </li>
+        <li>
+          Tailored Solutions:** We understand that every business is unique. Our
+          solutions are customized to align with your specific goals, ensuring
+          maximum impact and relevance.
+        </li>
+        <li>
+          Cutting-Edge Technology:** We stay at the forefront of technological
+          advancements. Our team leverages the latest tools and methodologies to
+          deliver state-of-the-art analytics solutions.
+        </li>
+        <li>
+          Data Security:** Your data's security is our top priority. We
+          implement robust measures to safeguard your information, providing you
+          with peace of mind.
+        </li>
       </ul>
     </section>
   );
